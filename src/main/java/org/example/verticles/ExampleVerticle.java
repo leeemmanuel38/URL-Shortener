f5f0401dd5dd12b4a90f64a421f5565903b57440
leeemmanuel38 <leeemmanuel38@gmail.com>
2022-02-25 20:38:30 -0500

    Added HTTP server

--- src/main/java/org/example/verticles/ExampleVerticle.java
+++ src/main/java/org/example/verticles/ExampleVerticle.java
@@ -6,6 +6,21 @@ import io.vertx.core.Future;
 public class ExampleVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> startFuture) {
-        System.out.println("Welcome to Vertx");
+        /* start the HTTP server
+         * requestHandler() method is called every time the server receives a request
+         */
+        vertx.createHttpServer()
+                .requestHandler(r -> r.response().end("Welcome to Vert.x")).listen(config().getInteger("http.port", 9090), result -> {
+            if (result.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(result.cause());
+            }
+        });
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Shutting down app");
     }
 }
